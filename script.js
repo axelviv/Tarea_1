cc190f6a5d29e9f230bd845402630b868e4d2651
axelviv <axelviv@live.com.ar>
2024-08-19 19:12:07 -0300

    tarea 1

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('claro').addEventListener('click', function() {
+    document.getElementById('css').setAttribute('href', 'formulario.css');
+});
+
+document.getElementById('contraste').addEventListener('click', function() {
+    document.getElementById('css').setAttribute('href', 'formulario2.css');
+});

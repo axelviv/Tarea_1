45eec55e85958a232be6dd1b9b356cf7fcc9f7ec
axelviv <axelviv@live.com.ar>
2024-09-19 23:16:58 -0300

    actividad clase 5 - implementación de bootstrap

---
+++ tema.js
@@ -0,0 +1,7 @@
+const temaOscuro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+}
+
+const temaClaro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+}
